088f9ffc489dff0c9d8f48546e135bc2971cc8d1
ko-morishita-clinks <ko-morishita@clinks.jp>
2025-09-30 12:25:53 +0900

    feat: sec5

---
+++ nuxt3-view/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <slot />
+    <footer>&copy; proglus.jp</footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  color: red;
+}
+
+footer {
+  background-color: orange;
+  color: white;
+  text-align: center;
+}
+
+html {
+  font-family: "Permanent Marker", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
---
+++ nuxt3-view/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      Top Page
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
