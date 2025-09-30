8d740342d1bb9b50e7fb51d356f977e23bc5f59d
ko-morishita-clinks <ko-morishita@clinks.jp>
2025-09-30 14:34:39 +0900

    feat: sec8

---
+++ nuxt3-usestate/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="titleState.changeTitle('Hello Nuxt3!')">
+      changeTitle
+    </button>
+  </div>
+</template>
+<script setup>
+  const titleState = useTitle()
+  const { title } = titleState
+</script>
