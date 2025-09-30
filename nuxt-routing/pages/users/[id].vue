c115a4918b28c37c20c34b0b756d1b5a0e69230b
ko-morishita-clinks <ko-morishita@clinks.jp>
2025-09-30 12:03:20 +0900

    feat: sec4

---
+++ nuxt-routing/pages/users/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>users/{{ $route.params.id }}を表示中</h1>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    validate: async(route) => {
+      return /^\d+$/.test(route.params.id)
+    }
+  })
+</script>
+
+<style>
+
+</style>
